737e305f2db3a17ff33431cc60c3ddadd95f8772
Hieu <nguyenconghieu@gmail.com>
2023-02-16 09:59:42 +0700

    Add project files.

---
+++ LabAnimal/Cat.cs
@@ -0,0 +1,13 @@
+﻿namespace LabAnimal
+{
+    internal class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + System.Environment.NewLine + "MEEOW";
+        }
+    }
+}
---
+++ LabAnimal/Dog.cs
@@ -0,0 +1,13 @@
+﻿namespace LabAnimal
+{
+    internal class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + System.Environment.NewLine + "DJAAF";
+        }
+    }
+}
